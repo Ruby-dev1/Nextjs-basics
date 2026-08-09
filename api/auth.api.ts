1a50b1b48c937646a6e72a8d7c0113e3fd87d0b9
Rubina <rubinasubedi18@gmail.com>
2026-08-09 16:49:39 +0545

    feat: configure axios instance and interceptors

--- api/auth.api.ts
+++ api/auth.api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { TLogin,TSignup } from "@/types/auth.types";
-
+import api from ".";
 // Login
 export const login = async (data: TLogin) => {
   try {
-    const response = await axios.post(
-      "http://localhost:8080/api/v1/auth/login",
+    const response = await api.post(
+     "/auth/login",
       data
     );
 
