bb8d08fea0c493fead756c63c5cd05b13e3263de
Rubina <rubinasubedi18@gmail.com>
2026-08-09 12:03:09 +0545

    add:SignUp schema

--- schemas/auth.schema.ts
+++ schemas/auth.schema.ts
@@ -5,3 +5,30 @@ export const loginSchema = yup.object({
   email: yup.string().email().required("email is required"),
   password: yup.string().required("password is required"),
 });
+
+
+//* register/signup  Schema
+
+export const signupSchema = yup.object({
+  fullName:yup
+  .string()
+  .required("fullName is required"),
+
+  email: yup
+  .string()
+  .email("Invalid email")
+  .required("email is required"),
+
+  phone: yup.string().required("phone number is required"),
+
+  password: yup
+  .string()
+  .min(6,"password must be atleast 6 characters long")
+  .required("password is required"),
+
+  confirmPassword:yup
+  .string()
+  .oneOf([yup.ref("password")],"password must match")
+  .required(" confirm password is required")
+
+})
