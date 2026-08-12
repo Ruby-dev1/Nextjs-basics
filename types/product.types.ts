ce407e526fdd1b5d744d12619d3134287b43279b
Rubina <rubinasubedi18@gmail.com>
2026-08-12 11:50:10 +0545

    product types defined

--- types/product.types.ts
+++ types/product.types.ts
@@ -1,4 +1,17 @@
 import * as yup from "yup"
 import { productSchema } from "@/schemas/product.schema"
+import { Image } from "./global.types"
 
 export type TProduct = yup.InferType<typeof productSchema>
+
+export type IProduct = {
+    _id:string,
+    name:string,
+    cover_image:Image,
+    images:Image[],
+    description?:string,
+    is_featured:boolean,
+    new_arrivals:boolean,
+    
+
+}
