750b1c56e9100a653c5b3c0e55b06a2b87431800
Rubina <rubinasubedi18@gmail.com>
2026-08-12 12:03:02 +0545

    integrate product api

--- api/product.api.ts
+++ api/product.api.ts
@@ -5,3 +5,30 @@ export const createProduct = async (data: FormData) => {
 
   return response.data;
 };
+
+//* get all products
+
+export const getAllProducts = async()=>{
+
+  try{
+    const response = await api.get("/product");
+    return response?.data;
+
+  }catch(error:any){
+    throw error?.response?.data;
+  }
+}
+
+
+//* get featured products
+
+export const getFeaturedProducts = async()=>{
+
+  try{
+    const response = await api.get("/product/featured")
+    return response?.data;
+
+  }catch(error:any){
+    throw error?.response?.data;
+  }
+}
